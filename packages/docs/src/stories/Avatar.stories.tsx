7e0590f8e0b3fb8149356ec79ad6ca12936cfe8c
Matheus <matheusmendesreis@outlook.com>
2023-12-28 14:18:02 -0300

    ci: npm

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -1,5 +1,5 @@
+import { Avatar, AvatarProps } from '@mmrignite-ui/react'
 import type { Meta, StoryObj } from '@storybook/react'
-import { Avatar, AvatarProps } from '@ignite-ui/react'
 
 export default {
   title: 'Data display/Avatar',
